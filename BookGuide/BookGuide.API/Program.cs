02159f004faf0ca2bdb95c2c1de3af0b672484ba
Fay Saleh <faysaltuwaijri@gmail.com>
2026-03-26 09:50:36 +0300

    fix: books search on render

--- BookGuide/BookGuide.API/Program.cs
+++ BookGuide/BookGuide.API/Program.cs
@@ -3,19 +3,15 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 🔥 مهم جدًا (سبب مشكلة 500 عندك)
 builder.Services.AddHttpClient();
 
-// Database
 builder.Services.AddDbContext<BookGuideDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Controllers
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// CORS (لازم لـ Render)
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
@@ -31,6 +27,10 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
+
+// Swagger
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Middleware
 app.UseCors("AllowFrontend");
