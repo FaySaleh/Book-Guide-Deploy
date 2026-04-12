57e09c38d93fd53db3bfd804df2d09aec53d7cb3
Fay Saleh <faysaltuwaijri@gmail.com>
2026-04-12 12:08:04 +0300

    fix

--- book-guide-ui/src/environments/environment.ts
+++ book-guide-ui/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://book-guide-deploy-production.up.railway.app/api'
+  //apiBaseUrl: 'https://book-guide-deploy-production.up.railway.app/api'
+    apiBaseUrl: '  https://bookguide-api.onrender.com'
+
 };
